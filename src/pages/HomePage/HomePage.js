fea15d90145167077576b46be25c33ea07161741
la-ki <layarkostic94@gmail.com>
2020-08-27 14:50:22 +0200

    Initial commit

---
+++ src/pages/HomePage/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './HomePage.css';
+
+import Banner from '../../components/Banner/Banner';
+
+function HomePage() {
+    return (
+        <div className="homepage">
+            <Banner />
+        </div>
+    )
+}
+
+export default HomePage;
